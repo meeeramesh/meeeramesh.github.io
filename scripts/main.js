651a2c395d9d32fc9f94c0ee521bb432719b9d9f
Meera Ramesh <mr828@cornell.edu>
2023-12-20 19:29:32 -0500

    education tab + minor home changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
+
+  // Navbar scrolling animation
   $(".nav-link").on("click", function (event) {
-    console.log("hey");
     event.preventDefault();
 
     var target = $(this).data("target");
@@ -8,4 +9,32 @@ $(document).ready(function () {
 
     $("html, body").animate({ scrollTop: offset }, 800);
   });
+
+  // Tab switching
+  $(document).on('click', '.other-tab', function () {
+    $('.curr-tab').addClass('other-tab').removeClass('curr-tab');
+    $(this).addClass('curr-tab').removeClass('other-tab');
+  });
+
+  // Hide tab divider and show window
+  $("#educ-tab").on('click', function () {
+    $('#divider').addClass('hidden');
+    $('.work-window').addClass('remove');
+    $('.educ-window').removeClass('remove');
+  });
+  $("#work-tab").on('click', function () {
+    $('#divider').removeClass('hidden');
+    $('.educ-window').addClass('remove');
+    $('.work-window').removeClass('remove');
+  });
+
+  // Extracurriculars Tab Switching
+  $(document).on('click', '.ec-unclicked', function () {
+    $('.ec-item').removeClass('.ec-clicked');
+    $('.ec-item').addClass('.ec-unclicked');
+    $(this).addClass('.ec-clicked');
+  })
+
+
 });
+
